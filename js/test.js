33e9fa1087b56b5f7a240294e8e35cec40553e5c
Adam Morris <amorris@mistermorris.com>
2015-11-28 14:40:08 +0800

    see

--- js/test.js
+++ js/test.js
@@ -1,1 +1,17 @@
-alert('hi');
+this.SSWrapper = {};
+
+(function (SSWrapper) {
+    /*
+      
+    */
+	SSWrapper.app = function (ss) {
+       if (ss === undefined) {
+         SSWrapper.ss = SpreadsheetApp.getActiveSpreadsheet();
+       } else {
+         SSWrapper.ss = undefined;
+       }
+	}
+
+}(this.SSWrapper));
+
+SSWrapper.app();
