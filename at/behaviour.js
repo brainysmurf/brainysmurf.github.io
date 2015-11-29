56f09302dd36e30ff2e75b262152de0c6ca08f82
Adam Morris <amorris@mistermorris.com>
2015-11-29 17:56:44 +0800

    Update behaviour.js

--- at/behaviour.js
+++ at/behaviour.js
@@ -1,4 +1,4 @@
 function main() {
 	awtble.$container.before($('<button/>', {id:'newButton', text:'<i class="add-plus"></i> Add New One'}));
-	$('#newButton').prepend($('<i/>', {class:'add-plus'})).button();
+	$('#newButton').prepend($('<span/>', {class:'ui-icon-circle-plus'})).button();
 }
