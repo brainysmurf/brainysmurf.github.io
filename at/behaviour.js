4ffeb0fc5aa747cf0f39d19b9d0709c914e929d2
Adam Morris <amorris@mistermorris.com>
2015-11-29 18:10:06 +0800

    Update behaviour.js

--- at/behaviour.js
+++ at/behaviour.js
@@ -1,7 +1,8 @@
 function main() {
 	awtble.$container.before($('<button/>', {id:'newButton', text:'Add New'}));
 	awtble.$container.before($('<div/>', {id:"#addNewDialog", style: "display:none;", title:'Add New!'}));
-	$('#newButton').button({icons:{primary:'ui-icon-circle-plus'}}).on('click', function(e) {
+	$('#newButton').button({icons:{primary:'ui-icon-circle-plus'}});
+	$('#newButton').click(function() {
 		$('#addNewDialog').dialog("open");
 	});
 }
