8c1556401619daf2d59deb30a9c70fb1dd905e10
Adam Morris <amorris@mistermorris.com>
2015-11-29 19:36:06 +0800

    Update behaviour.js

--- at/behaviour.js
+++ at/behaviour.js
@@ -2,8 +2,8 @@ function main(embedUrl, prefill) {
 	awtble.embedUrl = embedUrl;
 	awtble.prefill = prefill;
 	console.log(awtble.$title);
-	awtble.$title.after($('<button/>', {id:'newButton', text:'Add New'}));
-	awtble.$title.after($('<div/>', {id:"addNewDialog", style: "display:none;", title:'Add New!'}));
+	awtble.$container.before($('<button/>', {id:'newButton', text:'Add New'}));
+	awtble.$container.before($('<div/>', {id:"addNewDialog", style: "display:none;", title:'Add New!'}));
 	$('#newButton').button({icons:{primary:'ui-icon-circle-plus'}});
 	$('#addNewDialog').dialog({autoOpen:false});
 	$('#newButton').click(function() {
