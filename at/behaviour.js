c3fae9df13b70ca337708e3f38764210d6ffa577
Adam Morris <amorris@mistermorris.com>
2015-11-29 22:05:01 +0800

    Update behaviour.js

--- at/behaviour.js
+++ at/behaviour.js
@@ -1,16 +1,15 @@
-function main(embedUrl, prefill) {
-	awtble.embedUrl = embedUrl;
+function main(url, prefill) {
+	awtble.url = url;
 	awtble.prefill = prefill;
 	console.log(awtble.$title);
 	awtble.$container.before($('<button/>', {id:'newButton', text:'Add New', style:'margin-bottom:10px'}));
 	awtble.$container.before($('<div/>', {id:"addNewDialog", style: "display:none;", title:'Add New!'}));
 	//$("#addNewDialog").append('hi');
-	//$("#addNewDialog").append($('<iframe/>', {src:embedUrl, width:760, frameborder: 0, marginheight:0, text:'Loading…'}));
+	//$("#addNewDialog").append($('<iframe/>', {src:url, width:760, frameborder: 0, marginheight:0, text:'Loading…'}));
 	$('#newButton').button({icons:{primary:'ui-icon-circle-plus'}});
 	//$('#addNewDialog').dialog({autoOpen:false, height:"auto", width:750, modal:true, draggable:false,show:"fold"});
 	$('#newButton').click(function() {
 		//$('#addNewDialog').dialog("open");
-		var w = window.open(embedUrl);
-		//$(w.document.body).html($('<iframe/>', {src:embedUrl, width:760, frameborder: 0, marginheight:0, text:'Loading…'}));
+		var w = window.open(url + '/viewform');
 	});
 }
