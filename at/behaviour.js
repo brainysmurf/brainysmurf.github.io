9beb41291f34c831a920d57beb69e63fbf9a14aa
Adam Morris <amorris@mistermorris.com>
2015-11-29 18:00:41 +0800

    Update behaviour.js

--- at/behaviour.js
+++ at/behaviour.js
@@ -1,4 +1,4 @@
 function main() {
-	awtble.$container.before($('<button/>', {id:'newButton', text:'Add New One'}));
+	awtble.$container.before($('<button/>', {id:'newButton', text:'Add New'}));
 	$('#newButton').button({icons:{primary:'ui-icon-circle-plus'}});
 }
