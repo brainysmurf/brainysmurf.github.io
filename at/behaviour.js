7b8da4c82ca7058978176de6aa4335f3012e2dfc
Adam Morris <amorris@mistermorris.com>
2015-11-29 18:11:25 +0800

    Update behaviour.js

--- at/behaviour.js
+++ at/behaviour.js
@@ -1,6 +1,6 @@
 function main() {
 	awtble.$container.before($('<button/>', {id:'newButton', text:'Add New'}));
-	awtble.$container.before($('<div/>', {id:"#addNewDialog", style: "display:none;", title:'Add New!'}));
+	awtble.$container.before($('<div/>', {id:"addNewDialog", style: "display:none;", title:'Add New!'}));
 	$('#newButton').button({icons:{primary:'ui-icon-circle-plus'}});
 	$('#newButton').click(function() {
 		$('#addNewDialog').dialog("open");
