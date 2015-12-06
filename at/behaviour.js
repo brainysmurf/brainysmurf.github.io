5007983724cab5cd72347348739b47bbbc0c54ac
Adam Morris <amorris@mistermorris.com>
2015-12-06 16:48:45 +0800

    Update behaviour.js

--- at/behaviour.js
+++ at/behaviour.js
@@ -3,4 +3,11 @@ function main(url, prefill) {
 	awtble.definePrefill(prefill);
 	awtble.updateUrl(url);
 	awtble.makeNewButton('Add New', "Fill out this form");
+	
+	var kind = $('#sidebackground').data('kind');
+	$('sidebackground').addClass(kind == 'Internal Record' ? 'ui-icon-document' : 'ui-icon-mail-closed')
+	
+	background:url(images/pdf-icon.gif) center right no-repeat;
+padding:0 16px 0 0;
+white-space:nowrap;
 }
