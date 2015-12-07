8deea35a4696f198b450c340aac0f2cbff3116b9
Adam Morris <amorris@mistermorris.com>
2015-12-07 11:17:59 +0800

    Update behaviour.js

--- at/behaviour.js
+++ at/behaviour.js
@@ -5,8 +5,12 @@ function main(url, prefill) {
 	awtble.makeNewButton('Add New', "Fill out this form");
 	
 	var kind = $('.sidebackground').data('kind');
-	$('.sidebackground[data-kind="Internal Record"]').addClass('ui-icon ui-icon-document');
-	$('.sidebackground[data-kind="Internal Communication"]').addClass('ui-icon ui-icon-mail-closed');
-	$('.sidebackground[data-kind="External Communication"]').addClass('ui-icon ui-icon-mail-closed');
-	debugger;
+	//$('.sidebackground[data-kind="Internal Record"]').addClass('ui-icon ui-icon-document');
+	//$('.sidebackground[data-kind="Internal Communication"]').addClass('ui-icon ui-icon-mail-closed');
+	//$('.sidebackground[data-kind="External Communication"]').addClass('ui-icon ui-icon-mail-closed');
+	$(".info").each(function (index, $info) {
+		if ($info.data('kind') == "Internal Record") {
+			$info.find('.sidebackground').addClass('ui-icon ui-icon-mail-closed');
+		}
+	});
 }
