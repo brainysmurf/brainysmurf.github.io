1dd477eb5eb8e3b3e1068f1d28d4a5bea7060f7a
Adam Morris <amorris@mistermorris.com>
2015-12-07 11:19:55 +0800

    Update behaviour.js

--- at/behaviour.js
+++ at/behaviour.js
@@ -8,9 +8,9 @@ function main(url, prefill) {
 	//$('.sidebackground[data-kind="Internal Record"]').addClass('ui-icon ui-icon-document');
 	//$('.sidebackground[data-kind="Internal Communication"]').addClass('ui-icon ui-icon-mail-closed');
 	//$('.sidebackground[data-kind="External Communication"]').addClass('ui-icon ui-icon-mail-closed');
-	$(".info").each(function (index, $info) {
-		if ($info.data('kind') == "Internal Record") {
-			$info.find('.sidebackground').addClass('ui-icon ui-icon-mail-closed');
+	$(".info").each(function (index) {
+		if ($(this).data('kind') == "Internal Record") {
+			$(this).find('.sidebackground').addClass('ui-icon ui-icon-mail-closed');
 		}
 	});
 }
