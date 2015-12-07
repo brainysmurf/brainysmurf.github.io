bf7048349643b19164baaa0a3543112006bf19e0
Adam Morris <amorris@mistermorris.com>
2015-12-07 21:01:11 +0800

    Update behaviour.js

--- at/behaviour.js
+++ at/behaviour.js
@@ -4,13 +4,13 @@ function main(url, prefill) {
 	awtble.updateUrl(url);
 	awtble.makeNewButton('Add New', "Fill out this form");
 	
-	$(".info").each(function (index) {
-		$(this).find('.js-student-info').text( $(this).data('student') );
-		$(this).find('.js-student-extra-info').text( $(this).data('grade') );
+	$(".wrapper").each(function (index) {
+		$(this).find('.js-student-info').text( $(this).data('n') );
+		$(this).find('.js-student-extra-info').text( $(this).data('o') );
 		if ($(this).data('kind') == "Internal Record") {
 			$(this).find('.sidebackground').addClass('ui-icon ui-icon-document');
 			$(this).find('.js-content-title').text('Record:');
-			$(this).find('.js-content-body').text( $(this).data('content') );
+			$(this).find('.js-content-body').text( $(this).data('h') );
 		} else {
 			$(this).find('.sidebackground').addClass('ui-icon ui-icon-mail-closed')
 		}
