d692f8d96a6dd77e81a8e98c5187bf12d91d6b86
Adam Morris <amorris@mistermorris.com>
2015-12-08 14:46:51 +0800

    Update behaviour.js

--- at/behaviour.js
+++ at/behaviour.js
@@ -3,7 +3,8 @@ function main(url, prefill) {
 	awtble.definePrefill(prefill);
 	awtble.updateUrl(url);
 	awtble.makeNewButton('Add New', "Fill out this form");
-	
+	debugger;
+
 	$(".wrapper").each(function (index) {
 		$(this).find('.js-student-info').text( $(this).data('n') );
 		$(this).find('.js-student-extra-info').text( $(this).data('o') );
