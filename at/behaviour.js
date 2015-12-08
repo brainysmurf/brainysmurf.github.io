fa02088e79fe5a78ebe80ca759e2735268b5748f
Adam Morris <amorris@mistermorris.com>
2015-12-08 09:47:03 +0800

    Update behaviour.js

--- at/behaviour.js
+++ at/behaviour.js
@@ -7,12 +7,14 @@ function main(url, prefill) {
 	$(".wrapper").each(function (index) {
 		$(this).find('.js-student-info').text( $(this).data('n') );
 		$(this).find('.js-student-extra-info').text( $(this).data('o') );
-		if ($(this).data('kind') == "Internal Record") {
+		if ($(this).data('d') === "Internal Record") {
 			$(this).find('.sidebackground').addClass('ui-icon ui-icon-document');
 			$(this).find('.js-content-title').text('Record:');
 			$(this).find('.js-content-body').text( $(this).data('h') );
 		} else {
-			$(this).find('.sidebackground').addClass('ui-icon ui-icon-mail-closed')
+			$(this).find('.sidebackground').addClass('ui-icon ui-icon-mail-closed');
+			$(this).find('.js-content-title').text('Email:');
+			$(this).find('.js-content-body').text( $(this).data('j') + '<br/>' + $(this).data('l') );
 		}
 	});
 }
