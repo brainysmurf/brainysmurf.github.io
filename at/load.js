e33f611dbb15a69117e37ab3ca16e7eab8dff98b
Adam Morris <amorris@mistermorris.com>
2015-12-08 15:34:24 +0800

    Update load.js

--- at/load.js
+++ at/load.js
@@ -25,7 +25,15 @@ this.awtble = {};
 		awtble.$container.before($('<div/>', {id:"addNewDialog", style: "display:none;", title:dialogTitle}));
 		$("#addNewDialog").append($('<iframe/>', {src:awtble.embedUrl, height:"100%", width:"100%", frameborder: 0, marginheight:0, text:'Loading…'}));
 		$('#newButton').button({icons:{primary:'ui-icon-circle-plus'}});
-		$('#addNewDialog').dialog({autoOpen:false, height:700, width:"90%", modal:true, draggable:false,show:"fadeIn"});
+		$('#addNewDialog').dialog(
+			{autoOpen:false, 
+			height:700, 
+			width:"90%", 
+			modal:true, 
+			draggable:false,
+			show:"fadeIn"
+			position: { my: 'top', at: 'top+150' }
+		});
 		$('#newButton').click(function() {
 			$('#addNewDialog').dialog("open");
 		});
