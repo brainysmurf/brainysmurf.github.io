c0d24b6da73cdf6363157a2457e347801bfc30df
Adam Morris <amorris@mistermorris.com>
2015-12-11 22:50:26 +0800

    Update behaviour.js

--- at/behaviour.js
+++ at/behaviour.js
@@ -1,4 +1,5 @@
 function onLoad(element) {
+	console.log($(element));
 	$(element).find('.js-student-info').text( $(element).data('n') );
 	$(element).find('.js-student-extra-info').html( $(element).data('o') + '<br />' + $(element).data('a') );
 
