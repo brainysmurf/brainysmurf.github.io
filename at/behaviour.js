d930e7189b2e0e02613e4407c4c564f6280e0558
Adam Morris <amorris@mistermorris.com>
2015-12-11 22:46:57 +0800

    Update behaviour.js

--- at/behaviour.js
+++ at/behaviour.js
@@ -1,33 +1,36 @@
+function onLoad(element) {
+	if ($(this).data('d') === "Internal Record") {
+		$(this).find('.sidebackground').addClass('ui-icon ui-icon-document');
+		$(this).find('.js-content-title').text('Internal Record:');
+		$(this).find('.js-content-body').text( $(this).data('h') );
+	} else if ($(this).data('d') === "External Communication") {
+		$(this).find('.sidebackground').addClass('ui-icon ui-icon-mail-closed');
+		var emailText = '<p>' + $(this).data('l').split('\n').join('</p><p>') + '</p>';
+		$(this).find('.js-content-hide').replaceWith( $('<div/>', {
+			id:'emailText_'+index
+		}).addClass('white-popup mfp-hide').html(emailText) );
+		$(this).find('.js-content-title').html('Email to parents with subject <em>' + $(this).data('j') + '</em>:');
+		$(this).find('.js-content-body').html( $('<div/>', {
+				id:'emailTextPopup_'+index,
+			}).html(emailText)  //.magnificPopup({
+				//type: 'inline',
+				//midClick: true
+			//}) 
+		); // ends here
+	}
+}
+
 function modifyDom() {
 	console.log("modding");
 	$(".wrapper").each(function (index) {
 		console.log($(this).data('n'));
 		$(this).find('.js-student-info').text( $(this).data('n') );
 		$(this).find('.js-student-extra-info').html( $(this).data('o') + '<br />' + $(this).data('a') );
-		if ($(this).data('d') === "Internal Record") {
-			$(this).find('.sidebackground').addClass('ui-icon ui-icon-document');
-			$(this).find('.js-content-title').text('Internal Record:');
-			$(this).find('.js-content-body').text( $(this).data('h') );
-		} else if ($(this).data('d') === "External Communication") {
-			$(this).find('.sidebackground').addClass('ui-icon ui-icon-mail-closed');
-			var emailText = '<p>' + $(this).data('l').split('\n').join('</p><p>') + '</p>';
-			$(this).find('.js-content-hide').replaceWith( $('<div/>', {
-				id:'emailText_'+index
-			}).addClass('white-popup mfp-hide').html(emailText) );
-			$(this).find('.js-content-title').html('Email to parents with subject <em>' + $(this).data('j') + '</em>:');
-			$(this).find('.js-content-body').html( $('<div/>', {
-					id:'emailTextPopup_'+index,
-				}).html(emailText)  //.magnificPopup({
-					//type: 'inline',
-					//midClick: true
-				//}) 
-			); // ends here
-		}
+		onLoad(this);
 	});
 }
 
 function main(url, prefill) {
-	debugger;
 	awtble.definePrefill(prefill);
 	awtble.updateUrl(url);
 	awtble.makeNewButton('Add New', "Fill out this form");
@@ -35,9 +38,6 @@ function main(url, prefill) {
 	$('#controlers1').find('input')
 		.addClass('studentSearch')
 		.attr('placeholder', "Type to filter by Student");
-	$("#parentChart1").on('DOMAttrModified', function(e) {
-			modifyDom();
-		});
 	awtble.moveStringFilterToFront($('#controlers1'));
 	$('#controlers2').find('.charts-menu-button-caption').text("Filter by grade");
 
