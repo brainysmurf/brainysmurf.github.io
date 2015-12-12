9cd3f1e067435a157692dc556319aeb7a811fff9
Adam Morris <amorris@mistermorris.com>
2015-12-12 20:30:14 +0800

    almost there 2

--- at/behaviour.js
+++ at/behaviour.js
@@ -54,7 +54,7 @@ function main(url, prefill) {
 	modifyDom();
 	$(awtble.$container)
 		.observe('childList subtree', function(record) {
-			if (record.target.className == 'google-visualization-table' && record.addedNodes && record.addedNodes[0].firstChild.className === 'google-visualization-table-table') {
+			if (record.target.className == 'google-visualization-table' && record.addedNodes && record.addedNodes[0].firstChild && record.addedNodes[0].firstChild.className === 'google-visualization-table-table') {
 				console.log(record);
 				debugger;
 			}
