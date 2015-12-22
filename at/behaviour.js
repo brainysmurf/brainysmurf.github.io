2758632450dea46d0aa5375d8eebf1ef2d084503
Adam Morris <amorris@mistermorris.com>
2015-12-22 12:46:01 +0800

    observer

--- at/behaviour.js
+++ at/behaviour.js
@@ -136,7 +136,6 @@ function main(params) {
 	update();
 	$(awtble.$container)
 		.observe('childList subtree', function(record) {
-			console.log(record);
 			if (record.addedNodes && record.addedNodes.length == 1 && record.target.className == 'google-visualization-table') {
 				if (record.target.childNodes[0].childNodes[0].className === 'google-visualization-table-table') {
 					console.log(record);
